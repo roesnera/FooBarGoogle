24af78d6587304a8eef69f6df1d58ca01593d8ac
= <adam.roesner@generation.org>
2022-07-22 13:34:57 -0400

    Adds tests and .gitignore for out

--- test/SolutionTest.java
+++ test/SolutionTest.java
@@ -3,10 +3,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolutionTest {
 
     @org.junit.jupiter.api.Test
-    void solution() {
+    void solution1() {
         int act = Solution.solution(19,36);
         int exp = 1;
 
         assertEquals(exp,act);
+    }
+
+    void solution2() {
+        int act = Solution.solution(0,1);
+        int exp = 3;
+
+        assertEquals(exp, act);
     }
 }
